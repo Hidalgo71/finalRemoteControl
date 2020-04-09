7463ef541874445ebd4e7e00d27821354e777e9e
Hidalgo71 <y.ozdemir71@gmail.com>
2020-04-09 14:05:23 +0300

    Android bluetooth control app.

---
+++ consoleDesign/app/src/main/java/com/example/consoledesign/registerAct.java
@@ -0,0 +1,16 @@
+package com.example.consoledesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class registerAct extends Activity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_act_ly);
+    }
+}
